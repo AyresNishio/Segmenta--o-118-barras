f9a252f58509f088805a620080328a7215ccf377
Ayres Nishio <ayresnishio@gmail.com>
2021-10-05 14:08:13 -0300

    Ensaio sobreposção

--- mainsegmentador.py
+++ mainsegmentador.py
@@ -1,11 +1,11 @@
 from Segmentador.Segmenta import *
 
 Grupos = []
-G1 = np.loadtxt('Segmentador/Casos_feeling/Grupo1.txt',dtype=np.int32)
+G1 = np.loadtxt('Segmentador/Casos_feeling/Grupo1s.txt',dtype=np.int32)
 Grupos.append(G1)
-G2 = np.loadtxt('Segmentador/Casos_feeling/Grupo2.txt',dtype=np.int32)
+G2 = np.loadtxt('Segmentador/Casos_feeling/Grupo2s.txt',dtype=np.int32)
 Grupos.append(G2)
-G3= np.loadtxt('Segmentador/Casos_feeling/Grupo3.txt',dtype=np.int32)
+G3= np.loadtxt('Segmentador/Casos_feeling/Grupo3s.txt',dtype=np.int32)
 Grupos.append(G3)
 
 med_plan = np.loadtxt('Casos/118bLRG1/med118b333m.txt', dtype='i')
